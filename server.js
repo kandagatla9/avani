e46202a0fd94030d5c3e7833b24e0f1252c6d57c
kandagatla8 <srinath.kandagatla@manteio.in>
2022-02-19 15:00:19 +0530

    fdgfdg

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/AVANI'));
+app.use(express.static(__dirname + '/dist/Avani'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/AVANI/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Avani/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
